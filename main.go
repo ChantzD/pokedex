babab6560418e66b447a81070c9c3b623c048837
ChantzD <daltonc3@tutanota.com>
2025-05-24 12:48:45 -0400

    Fixed organization

--- main.go
+++ main.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
 )
 
 func main() {
-	for {
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		word := cleanInput(input)[0]
-		fmt.Println("Your command was:", word)
-	}
+	repl()
 }
--- repl.go
+++ repl.go
@@ -2,7 +2,21 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"fmt"
+	"os"
 )
+
+func repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		word := cleanInput(input)[0]
+		fmt.Println("Your command was:", word)
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
